7206662ada59f3a5148503a4844425590559e734
duda <dudamelimoliveira@gmail.com>
2024-10-30 17:29:54 +0000

    AV05

--- hello.py
+++ hello.py
@@ -1,7 +1,6 @@
-from flask import Flask, request, render_template, session, redirect, url_for, flash
+from flask import Flask, render_template, request, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -12,24 +11,19 @@ moment = Moment(app)
 app.config['SECRET_KEY'] = 'Chave Secreta'
 
 class NameForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
+    name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
-def form():
+def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
-        return redirect(url_for('form'))
-
+        return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'))
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
 
 @app.route('/user/<name>/<register>/<institution>')
 def user(name, register, institution):
@@ -37,11 +31,15 @@ def user(name, register, institution):
 
 @app.route('/contextorequisicao/<name>')
 def contexto_requisicao(name):
-    browser = request.headers.get('User-Agent')
+    user_agent = request.headers.get('User-Agent')
     ip = request.remote_addr
-    host = request.host
-    return render_template('contexto_requisicao.html',name=name, browser=browser, ip=ip, host=host)
+    host = request.headers.get('Host')
+    return render_template('contexto.html', name=name, user_agent=user_agent, ip=ip, host=host)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 @app.errorhandler(500)
-def page_internal_server_error(e):
+def internal_server_error(e):
     return render_template('500.html'), 500
