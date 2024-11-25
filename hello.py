1e8fd9034fdeee074bca0b0481f7e4d9509564dd
duda <dudamelimoliveira@gmail.com>
2024-11-25 14:11:34 +0000

    [AV09]

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -22,23 +22,26 @@ migrate = Migrate(app, db)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
+    form = UserForm()
+    form.populate_choices()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.name.data).first()
         if user is None:
-            user = User(username=form.name.data)
+            user = User(username=form.name.data, role_id=form.role.data)
             db.session.add(user)
             db.session.commit()
             session['known'] = False
         else:
             session['known'] = True
         session['name'] = form.name.data
+        session['role'] = form.role.data
         return redirect(url_for('index'))
-    users = User.query.all()  
+    users = User.query.all()
+    roles = Role.query.all()
+    users_count = User.query.count()
+    roles_count = Role.query.count()
     return render_template('index.html', form=form, name=session.get('name'),
-                           known=session.get('known', False), users=users)
-
-
+                           known=session.get('known', False), users=users, users_count=users_count, roles=roles, roles_count=roles_count)
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -46,23 +49,24 @@ class Role(db.Model):
     name = db.Column(db.String(64), unique=True)
     users = db.relationship('User', backref='role', lazy='dynamic')
 
-
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
-
-class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[DataRequired()])
+class UserForm(FlaskForm):
+    name = StringField('What is your name?:', validators=[DataRequired()])
+    role = SelectField('Role:', choices=[])
     submit = SubmitField('Submit')
 
+    def populate_choices(self):
+        from hello import Role
+        self.role.choices = [(role.id, role.name) for role in Role.query.all()]
 
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, Role=Role)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
